9ecaf9718c771936e2e7a1d7d81b2d0c0d2d4de5
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2021-07-25 20:52:54 +0700

    add mongodb driver

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module employee-golang
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.2 // indirect
+	go.mongodb.org/mongo-driver v1.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
 import (
+	"context"
 	Employees "employee-golang/employees"
 	Router "employee-golang/router"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
+	uri := "mongodb://localhost:27017"
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			fmt.Println("Error disconnect")
+			panic(err)
+		}
+	}()
+
+	// Ping the primary
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected and pinged.")
+
+	db := client.Database("employee")
+	collection := db.Collection("positions")
+
+	cur, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(ctx)
+
+	for cur.Next(ctx) {
+		var result bson.D
+		err := cur.Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// do something with result....
+		fmt.Println(result)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
 	Employees.Init()
 	Router.Init()
 }
